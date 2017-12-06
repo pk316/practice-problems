e504b72a366c06d682c8b2be9c4794e74b132ba1
pk316 <pr.khiev@gmail.com>
2017-12-06 13:10:01 -0800

    practice01

---
+++ practice01/main.js
@@ -0,0 +1,29 @@
+// ## Bigger words
+
+// Build a function that takes in a string and an array of strings.
+// The function should output an array of strings that are longer than the single input string.
+
+// - Write a function that takes two parameters
+//     - Parameter 1 - A string
+//         - Parameter 2 - An array of strings
+//             - The function should output all the words from the input array(parameter 2)
+//              that are longer than the input string(parameter 1)
+//                 - Example:
+// - `var myArray = ['bug', 'insect', 'mammal', 'reptile', 'mammoth', 'turtles'];`
+//     - `biggerWords('whales', myArray);`
+//     - Output - `['reptile', 'mammoth', 'turtles']`
+
+var arr = ['bug', 'insect', 'mammal', 'reptile', 'mammoth', 'turtles'];
+var str = 'whales';
+
+function biggerWords(str, arr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (str.length < arr[i].length) {
+            newArr.push(arr[i]);
+        }
+
+    }
+    console.log(newArr)
+}
+
